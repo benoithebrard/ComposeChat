9a739901892e112f2721b37005c2b7d5890eb1d2
Benoit Hebrard <benoit@shapegames.com>
2024-01-11 13:31:32 +0100

    scroll to top

--- app/src/main/java/com/example/composechat/ui/ChatPreviewEntries.kt
+++ app/src/main/java/com/example/composechat/ui/ChatPreviewEntries.kt
@@ -4,19 +4,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composechat.data.ChatUser
 import com.example.composechat.data.ChatUserPreview
 import com.example.composechat.ui.theme.ComposeChatTheme
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun ChatPreviewEntries(
     entries: List<ChatUserPreview>,
     onUserSelected: (ChatUser) -> Unit
 ) {
+    val lazyListState = rememberLazyListState()
+    LaunchedEffect(entries) {
+        snapshotFlow { lazyListState.firstVisibleItemIndex }
+            .collectLatest { index ->
+                if (index <= 1) {
+                    // scroll to top if the list changes while the top is visible
+                    lazyListState.scrollToItem(0)
+                }
+            }
+    }
+
     LazyColumn(
+        state = lazyListState,
         contentPadding = PaddingValues(10.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
